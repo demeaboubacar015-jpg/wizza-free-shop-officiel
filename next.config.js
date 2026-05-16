7a1785ce074060f3daacf857de9aff292c3e822c
Deme <demeaboubacar015@gmail.com>
2026-05-16 19:47:24 +0200

    fix: nettoyage de next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: {
-    // Ignore les erreurs ESLint pendant le build
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Ignore les erreurs TypeScript pendant le build
     ignoreBuildErrors: true,
   },
 }
