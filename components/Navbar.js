5ddfd39164caf39eef7d924e0e0ad7a409200b4f
Deme <demeaboubacar015@gmail.com>
2026-05-22 21:02:26 +0200

    ok

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,9 +2,12 @@ export default function Navbar() {
   return (
     <nav className="bg-black text-white p-4 flex justify-between">
       <h1 className="font-bold text-lg">Wizza Free Shop</h1>
-      <div>
-        <a href="/" className="mr-4">Home</a>
-        <a href="/about">About</a>
+      <div className="space-x-4">
+        <a href="/" className="hover:text-gold">Accueil</a>
+        <a href="/produits" className="hover:text-gold">Produits</a>
+        <a href="/contact" className="hover:text-gold">Contact</a>
+        <a href="/mentions-legales" className="hover:text-gold">Mentions légales</a>
+        <a href="/localisation" className="hover:text-gold">Localisation</a>
       </div>
     </nav>
   );
